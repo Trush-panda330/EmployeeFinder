484ba8adbf5b557b902c037c1cd942e81aec1dde
uetak330 <uetak330@gmail.com>
2024-09-27 22:56:07 +0900

    ドライバーテストクラスの作成

---
+++ jj_mysql/src/main/java/jj_mysql/TestJDBC.java
@@ -0,0 +1,35 @@
+package jj_mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJDBC {
+	/**
+	* JDBCデータベース接続テスト
+	* @param args
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	*/
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		//データベースdb1接続へのURL
+		final String url = "jdbc:mysql://localhost/db1?characterEncoding=UTF-8";
+		//DBのユーザー
+		final String username = "user1";
+		//DBのパスワード
+		final String password = "pass1";
+		//接続オブジェクト
+		Connection con = null;
+		//MySQLのJDBCドライバーをロードする
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Start");
+		try {
+			//接続の確立
+			con = DriverManager.getConnection(url, username, password);
+			System.out.println("接続成功！");
+		} finally {
+			con.close(); //接続を閉じておく
+		}
+	}
+
+}
