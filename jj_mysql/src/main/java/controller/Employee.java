98959c17e7692b0b5bf0b989fb6e9307351290b4
uetak330 <uetak330@gmail.com>
2024-09-29 00:35:56 +0900

    Employee.javaの作成
    文字コードの指定とgetParameterで
    index.jspからIDをとってこれているか一旦確認

---
+++ jj_mysql/src/main/java/controller/Employee.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Employee
+ */
+@WebServlet("/Employee")
+public class Employee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Employee() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//文字コード設定
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		// <input type="text" name="employee_id" /> index.jspのここでIDを受け取っている
+		String employee_id = request.getParameter("employee_id");
+		//コンソールに出してみて受け取れているか確認している
+		System.out.println("employee_id:" + employee_id);
+		
+		request.getRequestDispatcher("/WEB-INF/employee.jsp").forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
