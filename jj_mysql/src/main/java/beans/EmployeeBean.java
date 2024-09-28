63f6ffd8a281bb05da2ce0fab9f81091240bc3c4
uetak330 <uetak330@gmail.com>
2024-09-28 12:53:48 +0900

    社員Beanの作成

---
+++ jj_mysql/src/main/java/beans/EmployeeBean.java
@@ -0,0 +1,44 @@
+package beans;
+
+import java.io.Serializable;
+
+public class EmployeeBean implements Serializable {
+
+	private String employee_id;
+	private String employee_name;
+	private String department_name;
+	private String enter_date;
+
+	public String getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(String employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getEmployee_name() {
+		return employee_name;
+	}
+
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
+
+	public String getEnter_date() {
+		return enter_date;
+	}
+
+	public void setEnter_date(String enter_date) {
+		this.enter_date = enter_date;
+	}
+	
+}
